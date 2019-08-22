33c5a4a0b9fa137d84efdd0499d014247950c924
jjrocks <harrishjoshua1078@gmail.com>
2019-08-22 11:54:04 +0530

    1st .py file

---
+++ colors.py
@@ -0,0 +1,6 @@
+a=input("what color do u like the most?")
+if a !="blue" or a!="Blue" or a!="BLUE":
+    print("I like %s too"%a)
+    print("But i like blue more than that")
+else:
+    print("I too love %s,its my favourite"%a)
